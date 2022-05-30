9ec63a932eb30bf9420564cc4f51ce8b3109a459
Kyofu666 <99883154+Kyofu666@users.noreply.github.com>
2022-05-30 19:28:51 -0400

    header component changes

--- my-portfolio/src/components/header/Header.js
+++ my-portfolio/src/components/header/Header.js
@@ -1,10 +1,24 @@
 import "../../style/style.css";
 import React from "react";
-
+import HeaderBtn from "./headerBtn";
+import Me from "../../assets/Me.png";
+import HeaderSocials from "./headerSocials";
 function Header() {
   return (
-    <header>
-      <div className="container header__container"></div>
+    <header className="header">
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Wilems Rospide</h1>
+        <h5 className="text-light">Front-End Web Developer</h5>
+        <HeaderBtn />
+        <HeaderSocials />
+        <div className="header__image">
+          <img src={Me} alt="Wilems Rospide" />
+        </div>
+        <a href="#contact" className="header__scrollDown">
+          Scrool Down
+        </a>
+      </div>
     </header>
   );
 }
